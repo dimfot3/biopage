058d847d944edd2fd741d74fd3041f4e4a219b17
dimfot3 <fotiou.dimitris3@gmail.com>
2026-01-03 23:53:36 +0200

    upadated athletic page

--- src/data/athletic.js
+++ src/data/athletic.js
@@ -13,30 +13,30 @@ export const athleticProfile = {
   },
   raceHistory: [
     /* 2025 RACES */
-    { name: "Χειμερινός Ενιπέας", date: "12/14/2025", dist: "25", alt: "1740m", time: "03:35:35" },
+    { name: "Winter Enipeas", date: "12/14/2025", dist: "25", alt: "1740m", time: "03:35:35" },
     { name: "Thessaloniki Night Half Marathon", date: "10/18/2025", dist: "21", alt: "0m", time: "01:31:38" },
-    { name: "ΚΟΨΗ ΤΩΝ ΑΕΤΩΝ", date: "06/29/2025", dist: "14", alt: "450m", time: "01:41:40" },
+    { name: "Eagles Edge", date: "06/29/2025", dist: "14", alt: "450m", time: "01:41:40" },
     { name: "Vavdos Trail Run", date: "05/17/2025", dist: "22", alt: "1350m", time: "03:16:11" },
     { name: "Alexander the Great Marathon", date: "04/06/2025", dist: "42", alt: "0m", time: "03:45:04" },
     { name: "10 Cycle Trail Run", date: "02/09/2025", dist: "11", alt: "620m", time: "01:15:53" },
     
     /* 2024 RACES */
     { name: "Thermaikos Night Half", date: "11/02/2024", dist: "21", alt: "0m", time: "01:46:00" },
-    { name: "Τρέχω για την Κατερίνη", date: "10/13/2024", dist: "8", alt: "0m", time: "00:35:47" },
-    { name: "ΚΟΨΗ ΤΩΝ ΑΕΤΩΝ", date: "06/30/2024", dist: "14", alt: "450m", time: "02:05:03" },
-    { name: "Δρόμος Ανακτόρων", date: "04/07/2024", dist: "13", alt: "45m", time: "01:42:18" },
+    { name: "Run for Katerini", date: "10/13/2024", dist: "8", alt: "0m", time: "00:35:47" },
+    { name: "Eagles Edge", date: "06/30/2024", dist: "14", alt: "450m", time: "02:05:03" },
+    { name: "Palace Road", date: "04/07/2024", dist: "13", alt: "460m", time: "01:42:18" },
     
     /* 2023 RACES */
-    { name: "ΚΟΨΗ ΤΩΝ ΑΕΤΩΝ", date: "06/25/2023", dist: "14", alt: "450m", time: "02:04:17" },
+    { name: "Eagles Edge", date: "06/25/2023", dist: "14", alt: "450m", time: "02:04:17" },
     { name: "6ο Olympic Day Run", date: "04/02/2023", dist: "10", alt: "0m", time: "00:55:00" },
     
     /* 2022 RACES */
     { name: "Night Half (10k)", date: "10/15/2022", dist: "10", alt: "0m", time: "00:50:33" },
-    { name: "Τρέχω για την Κατερίνη", date: "10/09/2022", dist: "8", alt: "0m", time: "00:41:37" },
+    { name: "Run for Katerini", date: "10/09/2022", dist: "8", alt: "0m", time: "00:41:37" },
     { name: "5ο Olympic Day Run", date: "05/29/2022", dist: "10", alt: "0m", time: "00:54:55" },
     
     /* 2015 RACES */
-    { name: "Τρέχω για την Κατερίνη", date: "10/04/2015", dist: "8", alt: "0m", time: "00:54:10" },
+    { name: "Run for Katerini", date: "10/04/2015", dist: "8", alt: "0m", time: "00:54:10" },
   ],
   objectives: ["SUB-3:30 MARATHON", "50KM+ ULTRA TRAIL"]
 };
