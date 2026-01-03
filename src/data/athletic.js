9c39fb171ba15ca1ed8f12b8959a2b04817e00ed
dimfot3 <fotiou.dimitris3@gmail.com>
2026-01-03 23:55:13 +0200

    athletic info minor

--- src/data/athletic.js
+++ src/data/athletic.js
@@ -15,7 +15,7 @@ export const athleticProfile = {
     /* 2025 RACES */
     { name: "Winter Enipeas", date: "12/14/2025", dist: "25", alt: "1740m", time: "03:35:35" },
     { name: "Thessaloniki Night Half Marathon", date: "10/18/2025", dist: "21", alt: "0m", time: "01:31:38" },
-    { name: "Eagles Edge", date: "06/29/2025", dist: "14", alt: "450m", time: "01:41:40" },
+    { name: "Eagles' Edge", date: "06/29/2025", dist: "14", alt: "450m", time: "01:41:40" },
     { name: "Vavdos Trail Run", date: "05/17/2025", dist: "22", alt: "1350m", time: "03:16:11" },
     { name: "Alexander the Great Marathon", date: "04/06/2025", dist: "42", alt: "0m", time: "03:45:04" },
     { name: "10 Cycle Trail Run", date: "02/09/2025", dist: "11", alt: "620m", time: "01:15:53" },
@@ -23,11 +23,11 @@ export const athleticProfile = {
     /* 2024 RACES */
     { name: "Thermaikos Night Half", date: "11/02/2024", dist: "21", alt: "0m", time: "01:46:00" },
     { name: "Run for Katerini", date: "10/13/2024", dist: "8", alt: "0m", time: "00:35:47" },
-    { name: "Eagles Edge", date: "06/30/2024", dist: "14", alt: "450m", time: "02:05:03" },
+    { name: "Eagles' Edge", date: "06/30/2024", dist: "14", alt: "450m", time: "02:05:03" },
     { name: "Palace Road", date: "04/07/2024", dist: "13", alt: "460m", time: "01:42:18" },
     
     /* 2023 RACES */
-    { name: "Eagles Edge", date: "06/25/2023", dist: "14", alt: "450m", time: "02:04:17" },
+    { name: "Eagles' Edge", date: "06/25/2023", dist: "14", alt: "450m", time: "02:04:17" },
     { name: "6ο Olympic Day Run", date: "04/02/2023", dist: "10", alt: "0m", time: "00:55:00" },
     
     /* 2022 RACES */
